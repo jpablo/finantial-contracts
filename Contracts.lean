17a4ecc7e1039c08d83d82878b1f0c83b76e40b5
Juan Pablo Romero <jpablo.romero@gmail.com>
2021-09-23 09:13:43 -0700

    Add some data types

--- Contracts.lean
+++ Contracts.lean
@@ -1,2 +1,22 @@
+
+
+
 def main : IO Unit :=
   IO.println "Hello, world!"
+
+
+inductive Currency
+structure Obs (T: Type)
+
+inductive Contract
+  | zero: Contract
+  | one: Currency -> Contract
+  | give: Contract -> Contract
+  | and: Contract -> Contract -> Contract
+  | or:  Contract -> Contract -> Contract
+  | scale: Obs Float -> Contract -> Contract
+  | anytime: Contract -> Contract
+
+
+#check Currency
+#print Contract
